347e690570e6c4ab7582ed5556cc05a444ed9c43
pauruizmartinez <prm00@iesemilidarder.com>
2024-01-25 19:50:57 +0100

    Primera versió

---
+++ src/programa/Programa.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programa;
+
+/**
+ *
+ * @author paurv
+ */
+public class ClasseGit{
+ public static void main(String[] args) {
+   System.out.println("Aprendre a utilitzar git clone, git fetch i git pull");
+ }
+}
